5cca6da26e6e72fa35554a71b5ab5ae04040c338
bcheruiyot2221-create <bcheruiyot2221-create@users.noreply.github.com>
2026-09-10 15:12:18 +0300

    Initial app duplicate

---
+++ server/data/kit.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'Kit Starlink Standard',
+  price: 590,
+  currency: '$',
+  paymentHint: 'Paiement facile via Airtel Money',
+  description: 'Internet illimite • Partout en RDC',
+  features: [
+    'Ultra rapide - Internet sans interruption',
+    'Partout en RDC - Meme dans les zones reculees',
+    'Fiable - Meme sous la pluie',
+    'Installation facile - En quelques minutes'
+  ],
+  exclusiveOffer: 'Offre web exclusive - remise sur tous les kits',
+  bookingUrl: 'https://bbbb.onrender.com'
+};
